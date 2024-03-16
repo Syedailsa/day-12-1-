3a3f579de2909eaa26e2ac751bda025904f8b04f
Ilsa. <ilsasyeda@gmail.com>
2024-03-17 04:30:37 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,11 @@
+let pizzas: string[] = [
+    "pepparoni",
+    "cheesy beef",
+    "fajitta"
+];
+
+pizzas.forEach(pizza => {
+    console.log(`i love ${pizza} pizza!!`)
+});
+
+console.log("i like pizzas soo much");
